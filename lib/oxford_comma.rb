220be1de59b5a4a2e5423665b44e5be1a92e9739
Lisa Walters <campi_l@fortlewis.edu>
2020-08-25 03:48:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,12 @@ def oxford_comma(array)
 
   if array.length == 1
       puts array.join
-  
-  
+
+
     elsif
       array.length == 2
       puts ("#{array[0]} and #{array[1]}")
-      
+
   else
     final = array.pop
     counter = 0
@@ -15,7 +15,7 @@ def oxford_comma(array)
     sentence = array.join(", ")
     counter += 1
     end
-  
+
     sentence << ", and #{final}"
     puts sentence
   end
