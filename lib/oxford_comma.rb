99b7700d05f9961bc170a716add94b91048c4513
Lisa Walters <campi_l@fortlewis.edu>
2020-08-25 03:31:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,18 +2,17 @@ def oxford_comma(array)
 
   if array.length == 1
     puts array
-    
-  
+
+
   elsif
-  
+
   final = array.pop
   counter = 0
   while counter < array.length
   sentence = array.join(", ")
   counter += 1
   end
-  
+
   sentence << ", and #{final}"
   puts sentence
   end
-
