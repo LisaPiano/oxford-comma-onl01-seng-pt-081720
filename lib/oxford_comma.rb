ffc674526385082597969538b5a656583e0f3182
Lisa Walters <campi_l@fortlewis.edu>
2020-08-25 03:31:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,19 @@
 def oxford_comma(array)
 
-final = array.pop
+  if array.length == 1
+    puts array
+    
+  
+  elsif
+  
+  final = array.pop
+  counter = 0
+  while counter < array.length
+  sentence = array.join(", ")
+  counter += 1
+  end
+  
+  sentence << ", and #{final}"
+  puts sentence
+  end
 
-sentence = array.join(", ")
-
-sentence << (", and #{final}")
-sentence
-end
